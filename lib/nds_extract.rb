0afeb4f8dc63f2aa68621d19019bae8dff10e6f0
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-26 19:47:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -85,7 +85,7 @@ def gross_per_studio(collection)
      result[movie[:studio]] = movie[:worldwide_gross]
    else 
      result[movie[:studio]] += movie[:worldwide_gross]
-  
+   end
    
   i += 1
  end 
