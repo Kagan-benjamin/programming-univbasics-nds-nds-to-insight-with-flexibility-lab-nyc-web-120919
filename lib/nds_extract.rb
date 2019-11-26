5efb70835c16cb942a8e6052501a21f8316c8f1c
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-26 19:42:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,10 +75,11 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
 
-result = {} 
-i = 0 
- while i < collection.length do 
-  movie = collection[i]
+ result = {} 
+ i = 0 
+  
+  while i < collection.length do 
+   movie = collection[i]
   
    
   i += 1
